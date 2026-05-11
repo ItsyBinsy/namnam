c558c67fc8b1bec57e9de151cb3f90a1171670ca
DENISE <USER@LAPTOP-R8TA1BNI>
2026-05-11 18:01:59 +0800

    - added profile page - added restaurants - added reviews - fixed review display

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'profile.dart';
 import 'restaurant_detail.dart';
